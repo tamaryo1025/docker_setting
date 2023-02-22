23301861715326c01e55632653dd7aa79a461b8b
tamaryo1025 <tamaryo1025@gmail.com>
2023-02-23 01:10:02 +0900

    [modify] run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --name {CONTAINERNAME} --gpus all -p 8888:8888 -v /home/{USERNAME}/:/home/{USERNAME}/workdir {IMAGENAME}
+docker run -it --name {CONTAINERNAME} --gpus all -p 8888:8888 -v /mnt/data1/home/{USERNAME}/:/home/{USERNAME}/workdir {IMAGENAME}
